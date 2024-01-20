c94ec526b988e2680b4b794ed71b73f1bc6c7bfc
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-01-20 12:19:31 -0600

    Styled box, box now displays. State is defined from the array of colors. Handleclick now changes the colors and does not repeat the same color as previous

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -3,17 +3,46 @@
 // event listener
 
 import React, { Component } from 'react';
+import '../Box.css';
+import { choice, colors } from '../helpers';
 
 class ColorBox extends Component {
+    static defaultProps = {
+        allColors: colors
+    };
+
+
+
+    constructor(props){
+        super(props);
+        this.state = {color: choice(this.props.allColors)};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    chooseColor() {
+        let newColor;
+        do {
+            newColor = choice(this.props.allColors);
+        } while (newColor === this.state.color)
+        
+        this.setState({ color: newColor });
+    };
+
+    handleClick() {
+        this.chooseColor()
+    };
     
     render() {
         return (
             <div
                 className='Box'
-                style={{ backgroundColor: `#${this.props.color}`}}
+                style={{ backgroundColor: `#${this.state.color}` }}
+                onClick={this.handleClick}
             >
 
             </div>
         )
     }
-}
+};
+
+export default ColorBox;
